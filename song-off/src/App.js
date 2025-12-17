1eca042b1332d258aa224d0ef618fb1669b7089e
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2025-12-16 23:10:34 -0700

    init commit

---
+++ song-off/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+//import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+
+    <div> 
+      <Header />
+      <Home />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ song-off/src/pages/Home.js
@@ -0,0 +1,17 @@
+import './Home.css';
+
+  const Home = () =>  {
+
+	return (
+	  <div className="Home-box">
+        
+        <div className="Description-box">
+            <h2 className = "Description-title">Think you've got good Taste? Prove it.</h2>
+        </div>  
+
+	  </div>
+	);
+  }
+  
+  export default Home;
+  
