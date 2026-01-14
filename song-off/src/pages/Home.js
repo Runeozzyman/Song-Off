6e92ff4186c0d97ca1e2da88261a890380c7acc2
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-14 16:56:00 -0700

    merge fix

---
+++ song-off/src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css';
+
+  const Home = () =>  {
+
+	return (
+	 <div className="Home-box">
+  <div className="Content-box">
+    <h2 className="Description-title">Think you've got good Taste? Prove it.</h2>
+
+    <div className="input-row">
+      <h3 className="Display-input">Choose a display name to get started:</h3>
+      <input className="display-name" placeholder="Enter Display Name" />
+      <button className="submit-btn">Submit</button>
+    </div>
+  </div>
+</div>
+
+
+
+	);
+  }
+  
+  export default Home;
+  
