417ef8feaeb3f0de87be1cf415c639a73c4a2558
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-14 15:49:46 -0700

    routing continued

--- song-off/src/App.js
+++ song-off/src/App.js
@@ -4,24 +4,37 @@ import Home from './pages/Home';
 import App_Description from './components/App_Description';
 import BackgroundImage from './components/BackgroundImage';
 import Footer from './components/Footer';
+import Start from './pages/Start';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <BrowserRouter>
-        <div className='AppRoot'>
-          <Header />
-          <BackgroundImage/>
+  <div className="AppRoot">
+    <Header />
+    <BackgroundImage />
 
-          <div className='Boxes-wrapper'>
-            <App_Description />  
-            <Home />
-          </div>
+    <div className="Boxes-wrapper">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <App_Description />
+              <Home />
+            </>
+          }
+        />
 
-          <Footer/>
-        </div>
-    </BrowserRouter>
+        <Route path="start" element = {<Start />}></Route>
+
+      </Routes>
+    </div>
+
+    <Footer />
+  </div>
+</BrowserRouter>
   );
 }
 
