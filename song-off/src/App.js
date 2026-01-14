53f87b27b18b14442d702b73260dc7fc65f9d9e5
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-14 15:40:34 -0700

    Working routing
    -downgraded react-router-dom for functionality

--- song-off/src/App.js
+++ song-off/src/App.js
@@ -4,23 +4,24 @@ import Home from './pages/Home';
 import App_Description from './components/App_Description';
 import BackgroundImage from './components/BackgroundImage';
 import Footer from './components/Footer';
-//import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
+    <BrowserRouter>
+        <div className='AppRoot'>
+          <Header />
+          <BackgroundImage/>
 
-    <div className='AppRoot'>
-      <Header />
-      <BackgroundImage/>
+          <div className='Boxes-wrapper'>
+            <App_Description />  
+            <Home />
+          </div>
 
-      <div className='Boxes-wrapper'>
-        <App_Description />  
-        <Home />
-      </div>
-
-      <Footer/>
-    </div>
-
+          <Footer/>
+        </div>
+    </BrowserRouter>
   );
 }
 
