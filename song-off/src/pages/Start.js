2f763e695c91088637549c9170cca27c98f7c94b
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-19 14:30:32 -0700

    Updated landing + session storage for username

--- song-off/src/pages/Start.js
+++ song-off/src/pages/Start.js
@@ -1,9 +1,40 @@
 import React from 'react';
- 
+import { useState, useEffect } from 'react';
+
+
   const Start = () =>  {
+  
+  function generateRandomUsername(){
+    const adjectives = ["Swift", "Mellow", "Funky", "Groovy", "Chill", "Vibrant", "Lively", "Smooth"];
+    const nouns = ["Beats", "Rhythms", "Tunes", "Melodies", "Harmonies", "Vibes", "Chords", "Notes"];
+    const num = Math.floor(Math.random() * 1000);
+
+    const chosenAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const chosenNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    const user = chosenAdjective + chosenNoun + num;
+
+    return user
+  }
+  
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+
+    const storedUsername = sessionStorage.getItem("username");
+
+    if (storedUsername) {
+      setUsername(storedUsername);
+    } else {
+      const newUsername = generateRandomUsername();
+      sessionStorage.setItem("username", newUsername);
+      setUsername(newUsername);
+    }
+
+  }, []);
+    
 	return (
 	  <div>
-        <h1>Start Page</h1>
+        <h1>Start Page: Hello {username}</h1>
 	  </div>
 	);
   }
