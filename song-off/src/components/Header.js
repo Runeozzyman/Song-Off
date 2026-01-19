5da1c80f2a4685871342b95d73b48ceb5a7f05c3
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-19 14:33:51 -0700

    Header loops to home

--- song-off/src/components/Header.js
+++ song-off/src/components/Header.js
@@ -1,10 +1,20 @@
   import './component-css/Header.css';
+  import { Link } from 'react-router-dom';
 
   const Header = () =>  {
     
 	return (
 	  <div className="header">
-        <h1>Song Off</h1>
+        <Link to="/"
+
+          style={{ 
+            textDecoration: 'none', 
+            color: 'inherit'
+          }}
+          
+        >
+          <h1>Song Off</h1>
+        </Link>
         <h3>Think you've got good taste? Prove it.</h3>
 	  </div>
 	);
