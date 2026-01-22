f500ffd8dd0bbf79ad5ef869c2af658841af2c4c
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-01-21 17:08:03 -0700

    updated room nav logic

--- song-off/src/components/Room_Navigation.js
+++ song-off/src/components/Room_Navigation.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './component-css/Room_Navigation.css'
 import { Link } from 'react-router-dom';
+import { useNavigate }	from 'react-router-dom';
   
+
+
+
   const Room_Navigation = () =>  {
+
+	const navigate = useNavigate();
+
+	//asserts roomcode has been entered || TODO: add backend logic to verify with DB that room exists
+	function queryRoomCode(){
+		const roomCode = document.querySelector('.room-code-input').value;
+
+		if(roomCode == null || roomCode == ""){
+			alert("Please enter a room code to join a room.");
+			return;
+		}
+
+		navigate('/existing-room', {state: {roomCode: roomCode}});
+	}
+
+
+
 	return (
 	  <div className = "button-group">
 
@@ -10,10 +31,9 @@ import { Link } from 'react-router-dom';
 		<span>Create Room</span>
 		</Link>
 
-		<Link to = '/existing-room' className = "nav-button join-button">
-		<span>Join Room</span>
-		</Link>
-
+		<input type="text" placeholder = "Room Code" className = "room-code-input"></input>
+		<button className="nav-button join-button" onClick={queryRoomCode}><span>Join Room</span></button>
+		
 	  </div>
 	);
   }
