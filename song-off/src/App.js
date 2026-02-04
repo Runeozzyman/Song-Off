9286d435e490fa8237a7524bf6db79e11d65a71a
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-02-04 16:42:09 -0700

    working supabase connection

--- song-off/src/App.js
+++ song-off/src/App.js
@@ -3,12 +3,12 @@ import Header from './components/Header';
 import BackgroundImage from './components/BackgroundImage';
 import Footer from './components/Footer';
 import App_Routes from './routes/App_Routes';
-
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 
 function App() {
+
   return (
 <BrowserRouter>
   <div className="AppRoot">
