7b35fbecfd01636054f67b80962312a9a05cc337
Austin Wort <119460845+Runeozzyman@users.noreply.github.com>
2026-03-06 18:01:39 -0700

    update todo

--- song-off/src/components/App_Description.js
+++ song-off/src/components/App_Description.js
@@ -6,7 +6,7 @@ import './component-css/App_Description.css';
 	  <div className ="App-Description-box">
         <h2 className="App-Desc">What is Song Off?</h2>
         <p>Song Off is a weekly leaderboard where users submit their song of the week.
-           Every submission is tracked and the top ten songs of the week are shown on the leaderboard.
+           Every submission is tracked and the top 50 songs of the week are shown on the leaderboard.
            Show your favourite artists some love by supporting their music!
         </p>
         
